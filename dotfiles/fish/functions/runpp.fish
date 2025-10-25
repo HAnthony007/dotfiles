f0fb9f5470847e6ae74e6f001631102b1ab66434
HAnthony007 <harifetraanthonyr@gmail.com>
2025-10-25 11:51:33 +0300

    chore: remove dotfiles
    Removes all dotfiles related to fish, nvim, sway and waybar.

--- dotfiles/fish/functions/runpp.fish
+++
@@ -1,8 +0,0 @@
-function runpp
-    if test (count $argv) -eq 0
-        echo "Usage: runpp fichier.cpp"
-        return 1
-    end
-    set fichier (basename $argv[1] .cpp)
-    g++ $argv[1] -o $fichier && ./$fichier
-end
